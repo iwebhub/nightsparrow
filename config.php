6e22cd5cf5d9905986e70d0b521e90ae3dfa455e
Mario Borna Mjertan <bor.mbm@gmail.com>
2016-04-29 20:36:58 +0200

    fail

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('rootdirpath', '/Users/mbm/projekti/nightsparrow/');
-define('domainpath', 'http://ns.local/');
-define('mysql_user', 'root');
-define('mysql_database', 'ns');
-define('mysql_password', '');
-define('mysql_server', 'localhost');
